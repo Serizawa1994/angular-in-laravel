9a2597ee2237285f9e3a7f06b762a48f42fa9e58
Serry <riptonlemon@yahoo.co.jp>
2020-01-17 10:55:38 +0000

    install leader-line

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Inject, OnInit } from '@angular/core';
+import {DOCUMENT} from "@angular/common";
+import {LeaderLine} from "node_modules/leader-line"
+declare let leaderLine: any;
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.css']
+  styleUrls: ['./title.component.css'],
 })
 export class TitleComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document) { }
 
   ngOnInit() {
+    new LeaderLine (
+      this.document.getElementById('one'),
+      this.document.getElementById('two'),
+      {
+        startPlug: "behind",
+        endPlug: "behind",
+        startSocket: "bottom",
+        endSocket: "left"
+      }
+    );
+    
+    new LeaderLine (
+      this.document.getElementById('two'),
+      this.document.getElementById('three'),
+      {
+        startPlug: "behind",
+        endPlug: "behind",
+        startSocket: "bottom",
+        endSocket: "left"
+      }
+    );
   }
 
 }
